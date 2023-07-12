623c44fc32f4898702418b830383cc063efa2e91
abul <abulsiddiqui89@gmail.com>
2023-07-12 08:48:35 -0400

    CRUD

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import finch
 
 # Register your models here.
+
+admin.site.register(finch)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+#from .models import finches
 
-# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  finches = finch.objects.all()
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
+
+def finches_detail(request, finch_id):
+  finches = finch.objects.get(id=finch_id)
+  return render (request, 'finches/detail.html', {'finch' : finches})
+
+class finchCreate(CreateView):
+    model = finch
+    fields = '__all__'
+    success_url = '/finches'
+
+class finchUpdate(UpdateView):
+  model = finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class finchDelete(DeleteView):
+  model = finch
+  success_url = '/finches'
